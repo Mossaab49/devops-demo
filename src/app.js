ea596b8c34eb8a7e74c4e462b5f8a5b91c7b6467
Nigo <saouti.mossaab@etu.uae.ac.ma>
2026-04-04 22:34:26 +0100

    ok

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const tasksRouter= require('./routes/tasks');
+const tasksRouter = require('./routes/tasks');
 
 app.use(express.json());
 app.use('/tasks', tasksRouter);
